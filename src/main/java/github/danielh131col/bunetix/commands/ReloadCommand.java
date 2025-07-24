0a8088dc524c167079cd07a1dbd71e3bb821ed14
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-07-23 20:32:01 -0500

    Recode ReloadCommand (It's kind of weird hehe)

--- src/main/java/github/danielh131col/bunetix/commands/ReloadCommand.java
+++ src/main/java/github/danielh131col/bunetix/commands/ReloadCommand.java
@@ -3,6 +3,8 @@ package github.danielh131col.bunetix.commands;
 import github.danielh131col.bunetix.Bunetix;
 import github.danielh131col.bunetix.utils.CC;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 /**
@@ -16,13 +18,25 @@ public class ReloadCommand extends Command {
     private final Bunetix plugin;
 
     public ReloadCommand(Bunetix plugin) {
-        super("breload", "bunetix.reload");
+        super("breload");
         this.plugin = plugin;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof ProxiedPlayer) {
+            ProxiedPlayer player = (ProxiedPlayer) sender;
+            if (!player.hasPermission("bunetix.reload")) {
+                CC.noPermission(sender);
+                return;
+            }
+        }
+
         plugin.getFileManager().loadFiles();
-        sender.sendMessage(CC.translate("&a&lBUNETIX ⇨ &a¡Reload completado!"));
+
+        String msg = plugin.getFileManager().getMessages().getString("reload");
+        if (msg == null || msg.isEmpty()) msg = "&a&lBUNETIX ⇨ &a¡All files were reloaded!";
+
+        sender.sendMessage(new TextComponent(CC.translate(msg)));
     }
 }
