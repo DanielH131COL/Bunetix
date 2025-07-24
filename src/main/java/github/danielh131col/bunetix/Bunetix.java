c976a4bd16b54d5071ae6decc63bf74ccea32de7
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-07-23 19:46:04 -0500

    Added Message main to starting plugin.

--- src/main/java/github/danielh131col/bunetix/Bunetix.java
+++ src/main/java/github/danielh131col/bunetix/Bunetix.java
@@ -1,5 +1,7 @@
 package github.danielh131col.bunetix;
 
+import github.danielh131col.bunetix.commands.PingCommand;
+import github.danielh131col.bunetix.utils.CC;
 import github.danielh131col.bunetix.utils.FileManager;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -12,8 +14,25 @@ public final class Bunetix extends Plugin {
     public void onEnable() {
         instance = this;
 
+        CC.console("&8&m-----------------------------");
+        CC.console("");
+        CC.console("          &b&lBUNETIX");
+        CC.console("    &e&lENGLISH &7| &e&lINGLES");
+        CC.console("&fProxy Core is completely free.");
+        CC.console("&fYou can report bugs and more on Discord:");
+        CC.console("&e&nhttps://discord.gg/k2KwG37dbG");
+        CC.console("");
+        CC.console("    &6&lSPANISH &7| &6&lESPAÑOL");
+        CC.console("&fCore de Proxy Totalmente gratis.");
+        CC.console("&fPuedes reportar tus bugs y mas en Discord:");
+        CC.console("&6&nhttps://discord.gg/k2KwG37dbG");
+        CC.console("");
+        CC.console("&8&m-----------------------------");
+
         fileManager = new FileManager(this);
         fileManager.setup();
+
+        getProxy().getPluginManager().registerCommand(this, new PingCommand());
     }
 
     @Override
